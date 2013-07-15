2b2f2fa08a9f0626111a66bec689ab3de41edcec
dparalen <vetrisko@gmail.com>
2013-07-15 19:30:03 +0200

    fix: required to be a staticmethod

--- test/wikipedia_search_input_presence_test.py
+++ test/wikipedia_search_input_presence_test.py
@@ -4,7 +4,7 @@ from selenium_wrapper import SE
 import sys, unittest, re, time, os.path, logging, nose
 
 class OpenPageTestCase(unittest.TestCase):
-    search_input_locator = lambda : SE.find_element_by_name("search")
+    search_input_locator = staticmethod(lambda : SE.find_element_by_name("search"))
     def setUp(self):
         self.log = logging.getLogger("wikipedia.search.input.presence.test")
         self.verificationErrors = []
