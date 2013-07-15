1c52b9ffc8dc44b073241fb684fdf6b8e8769e42
dparalen <vetrisko@gmail.com>
2013-07-15 10:59:35 +0200

    introducing a simple test case

---
+++ test/wikipedia_search_input_presence_test.py
@@ -0,0 +1,17 @@
+from selenium_wrapper import SE
+
+
+import sys, unittest, re, time, os.path, logging, nose
+
+class OpenPageTestCase(unittest.TestCase):
+    search_input_locator = lambda : SE.find_element_by_name("search")
+    def setUp(self):
+        self.log = logging.getLogger("wikipedia.search.input.presence.test")
+        self.verificationErrors = []
+        SE.reset(url="http://en.wikipedia.org")
+    
+    def testSearchInputPresence(self):
+        self.assertIn(self.search_input_locator, SE)
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
