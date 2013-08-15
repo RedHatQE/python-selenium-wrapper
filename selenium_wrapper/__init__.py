644ab188fba53b2dccf4ff61b313df75f7b230a3
dparalen <vetrisko@gmail.com>
2013-08-15 11:33:42 +0200

    introducing helper context managers for navigating: current_url, restore_url

--- selenium_wrapper/__init__.py
+++ selenium_wrapper/__init__.py
@@ -2,4 +2,6 @@ from selenium_wrapper_class import SeleniumWrapper
 from screenshots import ScreenShots
 SE = SeleniumWrapper()
 
-__all__ = [SE, SeleniumWrapper, ScreenShots]
+from tools import restore_url, current_url
+
+__all__ = [SE, SeleniumWrapper, ScreenShots, restore_url, current_url]
---
+++ selenium_wrapper/tools.py
@@ -0,0 +1,18 @@
+from . import SE
+from contextlib import contextmanager
+
+@contextmanager
+def restore_url(url=None):
+    '''restore SE.current_url or given url'''
+    if url is None:
+        url = SE.current_url
+    yield
+    if SE.current_url != url:
+        SE.get(url)
+
+@contextmanager
+def current_url(url):
+    '''navigate given url and restore original url upon return'''
+    with restore_url():
+        SE.get(url)
+        yield
