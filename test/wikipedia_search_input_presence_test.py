2047ad496c292387d2966b6cfa2f90373ec75591
dparalen <vetrisko@gmail.com>
2013-08-26 15:30:42 +0200

    introducing test cases for current_url context manager

--- test/wikipedia_search_input_presence_test.py
+++ test/wikipedia_search_input_presence_test.py
@@ -1,4 +1,4 @@
-from selenium_wrapper import SE
+from selenium_wrapper import SE, restore_url, current_url
 
 
 import unittest
@@ -14,7 +14,29 @@ class OpenPageTestCase(unittest.TestCase):
         self.verificationErrors = []
     
     def testSearchInputPresence(self):
+        '''check input field presence'''
         self.assertIn(self.search_input_locator, SE)
+
+    def testCurrentUrlCtx(self):
+        '''check current url context manager works'''
+        with current_url('https://google.com'):
+            self.assertIn('google', SE.current_url)
+        self.assertIn('wikipedia', SE.current_url)
+
+    def testCurrentUrlCtxException(self):
+        '''check current url context manager works in case of exception'''
+        class CustomError(Exception):
+            '''a custom error type to catch'''
+
+        try:
+            with current_url('https://google.com'):
+                self.assertIn('google', SE.current_url)
+                raise CustomError("as expected")
+        except CustomError as e:
+            pass
+
+        self.assertIn('wikipedia', SE.current_url)
+        
 
     def tearDown(self):
         self.assertEqual([], self.verificationErrors)
